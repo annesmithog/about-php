4ae7ead6db7614da7e8df8ec2fd6f990c0ba99b3
annesmithog <annesmithog@gmail.com>
2025-10-06 21:24:51 +0900

    Fix

--- src/tmp.php
+++ src/tmp.php
@@ -3,21 +3,6 @@
 
 
 
-
-
-class Number {
-    public int $num1 = 1;
-    protected int $num2 = 2;
-    private int $num3 = 3;
-
-    function iterateVisible() {
-        foreach ($this as $key => $value) {
-            echo "{$key} => {$value}" . PHP_EOL;
-        }
-    }
-}
-
-$number = new Number();
-echo $number->iterateVisible();
-
+echo var_export(strrchr("abc", "b")) . PHP_EOL;  // "bc"
+echo var_export(strrchr("abc", "d")) . PHP_EOL;  // false
 
