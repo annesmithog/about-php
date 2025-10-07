49bfd8f9b7daaffa113bdf69f5db756d3f59c261
annesmithog <annesmithog@gmail.com>
2025-10-07 23:33:10 +0900

    Fix

--- src/models/Human.php
+++ src/models/Human.php
@@ -1,17 +1,17 @@
 <?php
 
-class Human {
-    public function __construct(string $name, int $age, string $country) {
-        $this->name = $name;
-        $this->age = $age;
-        $this->country = $country;
-    }
+// class Human {
+//     public function __construct(string $name, int $age, string $country) {
+//         $this->name = $name;
+//         $this->age = $age;
+//         $this->country = $country;
+//     }
     
-    public string $name;
-    public int $age;
-    public string $country;
+//     public string $name;
+//     public int $age;
+//     public string $country;
 
-    public function introduce(): string {
-        return "Hi, I'm {$this->name}.";
-    }
-}
+//     public function introduce(): string {
+//         return "Hi, I'm {$this->name}.";
+//     }
+// }
--- src/use_human.php
+++ src/use_human.php
@@ -1,11 +1,11 @@
 <?php
 
-spl_autoload_register(function ($class_name) {
-    $filename = __DIR__ . "/models/" . $class_name . ".php";
-    if (is_file($filename)) {
-        require_once $filename;
-    }
-});
+// spl_autoload_register(function ($class_name) {
+//     $filename = __DIR__ . "/models/" . $class_name . ".php";
+//     if (is_file($filename)) {
+//         require_once $filename;
+//     }
+// });
 
-$human = new Human("Anne", 20, "USA");
-echo $human->introduce() . PHP_EOL;     // Hi, I'm Anne.
+// $human = new Human("Anne", 20, "USA");
+// echo $human->introduce() . PHP_EOL;     // Hi, I'm Anne.
