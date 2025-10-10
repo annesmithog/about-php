8d5c259c89a2f76029e31d757948c9763c72d70d
annesmithog <annesmithog@gmail.com>
2025-10-10 15:41:28 +0900

    Fix

---
+++ tests/models/HumanT_est.php
@@ -0,0 +1,12 @@
+<?php
+
+// require_once __DIR__ . "/../../src/models/Human.php";
+
+// use PHPUnit\Framework\TestCase;
+
+// class HumanTest extends TestCase {
+//     public function testIntroduce() {
+//         $human = new Human("Anne", 25, "USA");
+//         $this->assertSame("Hi, I'm Anne.", $human->introduce());
+//     }
+// }
